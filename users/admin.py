981c823c56cc507b418b1db15aaedce40d39fb5b
Сергей <Serg18-124@yandex.ru>
2025-09-19 11:20:35 +0300

    Добавлены модели для users, recipes, collections_app, search. Зарегистрированы в админке.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    fieldsets = DjangoUserAdmin.fieldsets + (
+        ("Дополнительно", {"fields": ("avatar", "bio")}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+    search_fields = ("username", "first_name", "last_name", "email")
