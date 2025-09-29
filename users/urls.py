40b97ba64387f760e78fa991e9af8cd54fdba4fb
Trira <trubchik@mail.ru>
2025-09-29 07:27:27 +0300

    add  the 2nd part of frontenf templates

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('', views.index, name='users_index'),
+
+    # Временно закомментируем до создания представления
+
+  #  path('login/', views.login_view, name='login'),
+  #  path('logout/', views.logout_view, name='logout'),
+  #  path('register/', views.register_view, name='register'),
+  #  path('profile/', views.profile_view, name='profile'),
 ]
